c75cf34d940e6737c2a1d8262081cdb83f3d3afe
Pacski <remy.pachocinski@devinci.fr>
2020-10-09 23:03:16 +0200

    past routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::prefix('/command')->group(function(){
+    Route::get('', 'ApiCommandController@index')
+        ->name('api.commands.index');
+    Route::post('/{number}/{status}', 'ApiCommandController@changeStatus')
+        ->name('api.commands.change-status');
+});
+Route::prefix('/stock')->group(function(){
+    Route::get('', 'ApiStockController@index')
+        ->name('api.stocks.index'); 
+    Route::post('/addStock', 'ApiStockController@addStock')
+        ->name('api.stocks.addStock'); 
+    Route::post('/delete/{id}', 'ApiStockController@delete')
+        ->name('api.stocks.delete'); 
+    Route::get('/getAllType', 'ApiStockController@getAllType')
+        ->name('api.stocks.getAllType');
+});
+Route::prefix('/fabric')->group(function(){
+    Route::get('', 'ApiFabricController@index')
+        ->name('api.fabrics.index'); 
+    Route::post('/delete/{id}', 'ApiFabricController@delete')
+        ->name('api.fabrics.delete'); 
+
+});
+Route::prefix('/product')->group(function(){
+    Route::get('', 'ApiProductController@index')
+        ->name('api.products.index'); 
+    Route::get('/countProduct/{name}', 'ApiProductController@nbProduced')
+        ->name('api.products.nbProduced'); 
 });
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/','GeneralController@index')->middleware('auth')
+    ->name('pages.home.index');
+
+Route::prefix('/product')->group(function (){
+    Route::get('', 'ProductController@index')
+    ->name('pages.product.index');
+    Route::post('/addProduct', 'ProductController@create')
+    ->name('pages.product.create');
 });
+Route::prefix('/stock')->group(function (){
+    Route::get('', 'StockController@index')
+    ->name('pages.stock.index');
+    Route::post('addStock', 'StockController@create')
+    ->name('pages.stock.create');
+});
+Route::prefix('/fabric')->group(function (){
+    Route::get('', 'FabricController@index')
+    ->name('pages.fabric.index');
+    Route::post('addFabric', 'fabricController@create')
+    ->name('pages.fabric.create');
+});
+Route::prefix('/command')->group(function (){
+    Route::get('', 'CommandController@index')
+        ->name('pages.command.index');
+    Route::post('addCommand', 'CommandController@create')
+        ->name('pages.command.create');
+    Route::post('update', 'CommandController@updateStatus')
+        ->name('pages.command.updateStatus');
+    Route::post('addComment', 'CommandController@addComment')
+        ->name('pages.command.addComment');
+    Route::post('delete', 'CommandController@delete')
+        ->name('pages.command.delete');
+});
+Route::prefix('/my-account')->group(function (){
+    Route::get('', 'UserController@index')
+        ->name('pages.user.index');
+});
+
+// test vue js
+Route::prefix('/user')->group(function (){
+    Route::post('/create', 'UserController@create')
+        ->name('users.create');
+    Route::get('/index', 'UserController@index')
+        ->name('users.index');
+});
+//////////////////
+
+// test 2
+// User
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('user.login');
+Route::get('/logout', 'UserController@logout')->name('user.logout');
